635cc1274f108bd3e4c617b49353a0708d1d1f56
RJ Cabagnot <rjcabagnot@hotmail.com>
2021-04-11 23:05:42 +0200

    initial commit

--- src/services/trackerService.ts
+++ src/services/trackerService.ts
@@ -1,10 +1,9 @@
-import { trackerResponseModel } from "../models/trackerResponseModel";
 import api from "../api/axios";
 
 export async function getMonthAxios() {
-  return await api.get<trackerResponseModel>("api/v1/spots/month?region=usa");
+  return await api.get<any>("api/v1/spots/month?region=usa");
 }
 
 export async function getYearTrackerAxios() {
-  return await api.get<trackerResponseModel>("api/v1/spots/year?region=usa");
+  return await api.get<any>("api/v1/spots/year?region=usa");
 }
